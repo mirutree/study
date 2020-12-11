5bfc097c0d3e9f71ece0816a469eabf9e9a4a0b5
mirutree <63778557+mirutree@users.noreply.github.com>
2020-12-11 17:20:46 +0900

    성적계산 페이지 추가 폼데이터의 전달 방법과 한글 처리를 위한 인코딩 필터

--- spring01/src/main/java/com/example/spring01/controller/MainController.java
+++ spring01/src/main/java/com/example/spring01/controller/MainController.java
@@ -2,8 +2,11 @@ package com.example.spring01.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.spring01.model.dto.PointDto;
 
 @Controller
 public class MainController {
@@ -30,4 +33,23 @@ public class MainController {
 		model.addAttribute("result", result);
 		return "test/gugu_result";
 	}
+	
+	@RequestMapping("point.do")
+	public String point() {
+		return "test/point";
+	}
+	
+	@RequestMapping("point_result.do")
+	public String point_result(@ModelAttribute PointDto dto, Model model) {
+		dto.setTotal(dto.getKor() + dto.getEng() + dto.getMat());
+//		double average=dto.getTotal()/3.0;
+//		dto.setAverage(average);	// 평균 소수점 자리수가 길어짐
+		String average = String.format("%.2f", dto.getTotal()/3.0); //%.2f 소수이하 두자리 스트링으로 만든 후 다시 더블로
+		dto.setAverage(Double.parseDouble(average));
+		model.addAttribute("dto", dto);
+		return "test/point_result";
+	}
+	
+	
+	
 }
