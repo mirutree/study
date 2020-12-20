44c96f7aac9311e8719245e50e152b7240fa0016
mirutree <63778557+mirutree@users.noreply.github.com>
2020-12-21 03:11:41 +0900

    boardVO생성

---
+++ board/src/main/java/com/board/domain/boardVO.java
@@ -0,0 +1,17 @@
+package com.board.domain;
+
+public class boardVO {
+/*
+ create table tbl_board(
+  bno int not null auto_increment,
+  title varchar(50) not null,
+  content text not null,
+  writer varchar(30) not null,
+  regDate timestamp not null default now(),
+  viewCnt int default 0,
+  primary key(bno)
+);
+ */
+	
+	
+}
